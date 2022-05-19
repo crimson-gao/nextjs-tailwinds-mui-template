faf500bce9798ba3a2225ff34f146dd55feed1e5
gaohui.000 <gaohui.000@bytedance.com>
2022-05-19 17:50:55 +0800

    feat: init templates
    Change-Id: I664940e569127d4f3fd8a213cf642088f462947a

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
+import { ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import '../styles/globals.css'
+import { theme } from '../theme/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles)
+    }
+  }, [])
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
---
+++ tailwind.config.js
@@ -0,0 +1,62 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+// THIS OBJECT SHOULD BE SIMILAR TO ./theme/theme.js
+const themeConstants = {
+  paper: '#F9F9F9',
+  primary: {
+    main: '#fff',
+    dark: '#e5e5e5',
+  },
+  secondary: {
+    main: '#212121',
+    dark: '#3A3A3A',
+  },
+  error: {
+    main: '#b22222',
+    dark: '#8b0000',
+  },
+  fg: { main: '#fff', dark: 'rgba(55, 65, 81, 1)' },
+  breakpoints: {
+    xs: '0px',
+    mb: '350px',
+    sm: '600px',
+    md: '960px',
+    lg: '1280px',
+    xl: '1920px',
+  },
+}
+
+module.exports = {
+  mode: 'jit',
+  content: [
+    './pages/*.{js,ts,jsx,tsx}',
+    './Components/**/*.{js,ts,jsx,tsx}',
+    './hoc/*.{js,ts,jsx,tsx}',
+    './Widgets/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        paper: themeConstants.paper,
+        primary: themeConstants.primary,
+        secondary: themeConstants.secondary,
+        error: themeConstants.error,
+        fg: themeConstants.fg.main,
+        'fg-dark': themeConstants.fg.dark,
+      },
+    },
+
+    // We over ride the whole screens with breakpoints for width. The 'ha' breakpoint will help us in blocking hover animations on devices not supporting hover.
+    screens: {
+      ...defaultTheme.screens,
+      ...themeConstants.breakpoints,
+      ha: { raw: '(hover: hover)' },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
